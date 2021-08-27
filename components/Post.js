6868bc808af5d85afe49e55bf8b8218e7c4cda5c
Jared Jewell <jaje9434@colorado.edu>
2021-08-27 12:56:55 -0600

    (fix): temporarily(?) removed hero image

--- components/Post.js
+++ components/Post.js
@@ -4,7 +4,9 @@ export default function Post({ post }) {
   return (
     <div className="card">
 
-      <img src={post.frontmatter.cover_image} alt="" />
+      {
+        // <img src={post.frontmatter.cover_image} alt="" />
+      }
 
       <div className="post-date">Posted on {post.frontmatter.date}</div>
 
