3c5caf0069f951b44d847e062c5485e1f61220ea
Jared Jewell <jaje9434@colorado.edu>
2021-08-27 11:19:20 -0600

    (feat): added GH & LI icons to header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,15 @@
 import Link from 'next/link'
+import { GoMarkGithub } from 'react-icons/go'
+import { FaLinkedinIn } from 'react-icons/fa'
+
 export default function Header() {
   return (
     <header>
       <div className="container">
-        <Link href="/">Dev Blog</Link>
+        <Link href="/">Jared&apos;s Space</Link>
+        <a href="https://github.com/yairdyule"><GoMarkGithub className="react-icons" /></a>
+        <a href="https://www.linkedin.com/in/jared-jewell-542a181b9/"><FaLinkedinIn className="react-icons" /></a>
+
       </div>
     </header>
   )
