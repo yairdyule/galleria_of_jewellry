d98b56cb49ff0ed639f2b1d2bbc217ec1ab56e2d
Jared Jewell <jaje9434@colorado.edu>
2021-08-27 12:56:37 -0600

    (feat): cleaned up header

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,14 @@
 import Link from 'next/link'
 import { GoMarkGithub } from 'react-icons/go'
 import { FaLinkedinIn } from 'react-icons/fa'
+import { AiFillHome } from 'react-icons/ai'
 
 export default function Header() {
   return (
     <header>
-      <div className="container">
-        <Link href="/">Jared&apos;s Space</Link>
-        <a href="https://github.com/yairdyule"><GoMarkGithub className="react-icons" /></a>
-        <a href="https://www.linkedin.com/in/jared-jewell-542a181b9/"><FaLinkedinIn className="react-icons" /></a>
-
-      </div>
+      <Link href="/"><a className="react-icons"><AiFillHome /></a></Link>
+      <a href="https://github.com/yairdyule"><GoMarkGithub className="react-icons" /></a>
+      <a href="https://www.linkedin.com/in/jared-jewell-542a181b9/"><FaLinkedinIn className="react-icons" /></a>
     </header>
   )
 }
