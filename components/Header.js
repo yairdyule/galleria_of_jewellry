06b0e5dfed0260665c16f047df59f2c6bdd8560e
Jared Jewell <jaje9434@colorado.edu>
2021-08-28 11:35:10 -0600

    (feat): restyled, different colors, etc.

--- components/Header.js
+++ components/Header.js
@@ -6,9 +6,9 @@ import { AiFillHome } from 'react-icons/ai'
 export default function Header() {
   return (
     <header>
-      <Link href="/"><a className="react-icons"><AiFillHome /></a></Link>
-      <a href="https://github.com/yairdyule"><GoMarkGithub className="react-icons" /></a>
-      <a href="https://www.linkedin.com/in/jared-jewell-542a181b9/"><FaLinkedinIn className="react-icons" /></a>
+      <Link href="/"><a className="btn react-icons"><AiFillHome /></a></Link>
+      <a className="btn" href="https://github.com/yairdyule"><GoMarkGithub className="react-icons" /></a>
+      <a className="btn" href="https://www.linkedin.com/in/jared-jewell-542a181b9/"><FaLinkedinIn className="react-icons" /></a>
     </header>
   )
 }
