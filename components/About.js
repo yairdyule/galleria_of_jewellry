232383d560c5c9e53ce46db274dcce0e6daef2a6
Jared Jewell <jaje9434@colorado.edu>
2021-08-31 11:15:38 -0600

    (feat): Created About component

---
+++ components/About.js
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div className="about">
+      <div className="tidbit">
+        <h4>About me</h4>
+        <p>Howdy, I'm Jared! I'm a 4th-year computer science student here at CU Boulder, in Colorado. During my time here, I've discovered and fell in love with programming, as it is a source of never-ending challenge and opportunities for growth, learning, and potential!</p>
+      </div>
+    </div>
+  )
+}
