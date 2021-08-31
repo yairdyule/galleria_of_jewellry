d11a9f6324f93c7d8dc3d8d55b114def9522ddf4
Jared Jewell <jaje9434@colorado.edu>
2021-08-31 12:10:44 -0600

    (fix): &apos; in favor of '
    Signed-off-by: Jared Jewell <jaje9434@colorado.edu>

--- components/About.js
+++ components/About.js
@@ -4,20 +4,20 @@ export default function About() {
       <div className="tidbit">
         <h2>About me</h2>
         <div className="content">
-          <p>Howdy, I'm Jared! I'm a 4th-year computer science student here at CU Boulder, in Colorado. During my time here, I've discovered and fell in love with programming, as it is a source of never-ending challenge and opportunities for growth, learning, and excitement! I've had the opportunity to serve both as a Course and Teaching assistant, and work with a team of students on a full-stack social media web application.</p>
+          <p>Howdy, I&apos;m Jared! I&apos;m a 4th-year computer science student here at CU Boulder, in Colorado. During my time here, I&apos;ve discovered and fell in love with programming, as it is a source of never-ending challenge and opportunities for growth, learning, and excitement! I&apos;ve had the opportunity to serve both as a Course and Teaching assistant, and work with a team of students on a full-stack social media web application.</p>
 
           <br />
 
-          <p>In my free time, I love learning about web development, reading classic novels, and recreating outdoors. I'm also a sucker for mindfulness meditation, a practice which I find improves all aspects of my life.</p>
+          <p>In my free time, I love learning about web development, reading classic novels, and recreating outdoors. I&apos;m also a sucker for mindfulness meditation, a practice which I find improves all aspects of my life.</p>
         </div>
       </div>
       <div className="tidbit">
         <h2>Fun facts</h2>
         <ul>
           <li>I love workspace ergonomics!</li>
-          <li>I'm a (neo)vim geek—I love configuring my IDE!</li>
+          <li>I&apos;m a (neo)vim geek—I love configuring my IDE!</li>
           <li>I use Colemak, a non-QWERTY keyboard layout designed for ergonomics.</li>
-          <li>I'm AIARE I certified, meaning I'm qualified to assess avalanche conditions in backcountry skiing terrain!</li>
+          <li>I&apos;m AIARE I certified, meaning I&apos;m qualified to assess avalanche conditions in backcountry skiing terrain!</li>
         </ul>
       </div>
     </div>
