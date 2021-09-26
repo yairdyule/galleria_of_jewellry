4019b9bda6732a360a36abc97851da99f8c09261
Jared Jewell <jaje9434@colorado.edu>
2021-09-25 21:01:01 -0600

    cleaning css & formatting

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,59 +1,73 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import marked from 'marked'
-import Link from 'next/link'
-import Header from '../../components/Header'
-import { FaChevronLeft } from 'react-icons/fa'
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import marked from "marked";
+import Link from "next/link";
+import Header from "../../components/Header";
+import { FaChevronLeft } from "react-icons/fa";
 
-export default function PostPage({ frontmatter: { title, date, cover_image }, slug, content }) {
+export default function PostPage({
+  frontmatter: { title, date, cover_image },
+  slug,
+  content,
+}) {
   return (
     <>
       <Header />
       <div className="card card-page">
-        <Link href="/" ><a className="btn"><FaChevronLeft className="react-icons" /></a></Link>
-        <h1 className="post-title">{title}</h1>
-        <p className="post-date">Posted on {date}</p>
-        <img src={cover_image} className="cover_image" alt="The cover image for this article." />
+        <Link href="/">
+          <a className="btn">
+            <FaChevronLeft className="react-icons" />
+          </a>
+        </Link>
+        <div className="container">
+          <img
+            src={cover_image}
+            className="cover_image"
+            alt="The cover image for this article."
+          />
+          <div className="overlay">
+            <h1 className="post-title">{title}</h1>
+            <p className="post-date">Posted on {date}</p>
+          </div>
+        </div>
+
         <div className="post-body">
           <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  const files = fs.readdirSync(path.join('posts')) //get files
+  const files = fs.readdirSync(path.join("posts")); //get files
 
   //create their corresponding paths
-  const paths = files.map((filename) => (
-    {
-      params: {
-        slug: filename.replace('.md', '')
-      }
-    }
-  ))
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace(".md", ""),
+    },
+  }));
 
   console.log(paths);
 
   return {
     paths,
-    fallback: false // 404 when attempting to access slugs that don't exist
-  }
+    fallback: false, // 404 when attempting to access slugs that don't exist
+  };
 }
 
 export async function getStaticProps({ params: { slug } }) {
+  const mdWithMeta = fs.readFileSync(path.join("posts", slug + ".md"), "utf-8");
 
-  const mdWithMeta = fs.readFileSync(path.join('posts', slug + '.md'), 'utf-8')
-
-  const { data: frontmatter, content } = matter(mdWithMeta)
+  const { data: frontmatter, content } = matter(mdWithMeta);
 
   return {
     props: {
       frontmatter,
       slug,
-      content
-    }
-  }
+      content,
+    },
+  };
 }
