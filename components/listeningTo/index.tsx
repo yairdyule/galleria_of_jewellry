fa75171f4feaf68bcaf4ffa3b31d21e775edc660
Jared Jewell <jaje9434@colorado.edu>
2022-05-28 17:19:23 -0600

    create components

---
+++ components/listeningTo/index.tsx
@@ -0,0 +1,24 @@
+import { ICurrentlyPlaying } from "../../pages/about";
+
+interface ListeningToProps {
+  currently_playing: ICurrentlyPlaying;
+}
+
+export default function ListeningTo({ currently_playing }: ListeningToProps) {
+  if (!currently_playing.is_playing) {
+    return (
+      <h3 className="text-neutral-400">
+        Oddly enough, I'm not listening to anything currently.
+      </h3>
+    );
+  }
+
+  return (
+    <h3 className="text-neutral-500 text-sm transition-all duration-100 hover:scale-100">
+      Listening to:{" "}
+      <a className='text-emerald-400' href={currently_playing.href} target="_blank">
+        {currently_playing.name} - {currently_playing.artists}
+      </a>
+    </h3>
+  );
+}
---
+++ components/topTracks/index.tsx
@@ -0,0 +1,27 @@
+import { ITopTracks } from "../../pages/about";
+
+interface TopTracksProps {
+  tracks: ITopTracks[];
+}
+
+export default function TopTracks({ tracks }: TopTracksProps) {
+  return (
+    <div className="flex-col gap-1 items-center justify-center">
+      <h3 className="text-sm text-neutral-600 border-b border-b-gray-300 mb-1 pb-1">
+        Recent Favorites
+      </h3>
+      <ul>
+        {tracks &&
+          tracks.map((t, i) => {
+            return (
+              <a key={i} href={t.href}>
+                <li className="text-xs text-neutral-500 transition hover:text-emerald-400 hover:translate-x-1">
+                  {t.name} - {t.artists}
+                </li>
+              </a>
+            );
+          })}
+      </ul>
+    </div>
+  );
+}
