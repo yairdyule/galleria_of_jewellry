67874c3ce243748ad588ac35decdd4d90fa38630
Jared Jewell <jaje9434@colorado.edu>
2022-05-28 23:40:02 -0600

    update styling

--- components/breadCrumbs/index.tsx
+++ components/breadCrumbs/index.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 
 const Crumb = ({ href, isLast = false }: { href: string; isLast: boolean }) => {
+
   if (isLast) {
     return <li className="text-emerald-400">{"/" + href}</li>;
   }
 
   return (
     <Link passHref href={href === "home" ? "/" : "/" + href}>
-      <a>
+      <a className='hover:text-emerald-400'>
         <li>{"/" + href}</li>
       </a>
     </Link>
@@ -16,7 +17,7 @@ const Crumb = ({ href, isLast = false }: { href: string; isLast: boolean }) => {
 
 export const BreadCrumbs = ({ path }: { path: string[] }) => {
   return (
-    <ul className="flex flex-row text-sm text-neutral-500">
+    <ul className="flex flex-row text-md text-neutral-500 pb-2 justify-start items-start ml-1 w-full">
       {path.map((segment, ind) => {
         return (
           <Crumb key={ind} href={segment} isLast={ind === path.length - 1} />
