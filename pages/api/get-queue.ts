e3b429e56c3bf046e005ec7f4bb676b184139dd4
Jared Jewell <jaje9434@colorado.edu>
2022-05-29 17:44:47 -0600

    create database utility functions

---
+++ pages/api/get-queue.ts
@@ -0,0 +1,15 @@
+import { getQueue } from "./lib/database";
+import { getTracksByIds } from "./lib/spotify";
+
+export type QueueEntry = { id: string; song_id: string };
+
+export default async (_: any, res: any) => {
+  const data = await getQueue();
+  const {tracks} = await getTracksByIds(data.map((q) => q.song_id));
+
+  if (!tracks) {
+    return res.status(200).json({ queue: [] });
+  }
+
+  return res.status(200).json(tracks);
+};
---
+++ pages/api/lib/database.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@prisma/client";
+
+export const db = new PrismaClient();
+
+export const connect = async () => {
+  await db.$connect();
+  return db;
+};
+
+export const disconnect = async () => {
+  await db.$disconnect();
+};
+
+export const getLastListenedTo = async () => {
+  const data = await db.last_listening_to.findMany({ where: {} });
+  return data[0];
+};
+
+export const updateListeningTo = async (song_id: string) => {
+  const last_id = await getLastListenedTo();
+  await db.last_listening_to.update({
+    where: {
+      id: last_id.id,
+    },
+    data: {
+      song_id,
+    },
+  });
+};
+
+export const getQueue = async () => {
+  const q = await db.queue.findMany()
+  return q
+}
