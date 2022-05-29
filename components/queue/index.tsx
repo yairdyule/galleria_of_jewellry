65935c952952c847239e2dfcbc2b97e9e7af23f1
Jared Jewell <jaje9434@colorado.edu>
2022-05-29 17:46:00 -0600

    create queue, update jams w/ Queue

---
+++ components/queue/index.tsx
@@ -0,0 +1,120 @@
+import { Combobox, Dialog } from "@headlessui/react";
+import React from "react";
+import { GrAdd } from "react-icons/gr";
+import { Song } from "../../pages/api/lib/spotify.d";
+import { SearchResult, searchSong } from "../../pages/services";
+
+interface QueueProps {
+  queue: Song[];
+}
+
+export default function Queue({ queue }: QueueProps) {
+  const [modalOpen, setModalOpen] = React.useState(false);
+  return (
+    <div className="pt-6 flex-col gap-1 items-center justify-center">
+      <h3 className="text-lg  border-b-2 border-b-emerald-300 mb-1 pb-1">
+        Visitor Suggestions
+      </h3>
+      <ul>
+        {queue &&
+          queue
+            .reverse()
+            .slice(0, 10)
+            .map((q, i) => {
+              const artists = q.artists.map((a) => a.name).join(", ");
+              return (
+                <a key={i} href={q.href} target="_blank" rel="noreferrer">
+                  <li className="text-md text-neutral-600 transition hover:text-emerald-400 hover:translate-x-1 border-b border-b-slate-300">
+                    {q.name} - {artists}
+                  </li>
+                </a>
+              );
+            })}
+        <li className="text-md h-5 text-neutral-600 transition hover:text-emerald-400 hover:translate-y-1 flex flex-row items-center justify-center">
+          <GrAdd className="text-md" onClick={() => setModalOpen(true)} />
+        </li>
+      </ul>
+
+      {modalOpen && (
+        <AddSongModal isOpen={modalOpen} setIsOpen={setModalOpen} />
+      )}
+    </div>
+  );
+}
+
+function AddSongModal({
+  isOpen,
+  setIsOpen,
+}: {
+  isOpen: boolean;
+  setIsOpen: (a: boolean) => void;
+}) {
+  const [selectedSong, setSelectedSong] = React.useState("");
+  const [query, setQuery] = React.useState("");
+  const [results, setResults] = React.useState<SearchResult[]>([]);
+
+  const filteredResults =
+    query === ""
+      ? results
+      : results.filter(({ name, artists }) => {
+        return testQuery(query, name + " " + artists);
+      });
+
+  React.useEffect(() => {
+    searchQuery(query);
+  }, [query]);
+
+  const searchQuery = async (query: string) => {
+    if (query !== "") {
+      let data = await searchSong(query);
+      setResults(data);
+    }
+  };
+
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={() => setIsOpen(false)}
+      className="relative z-50"
+    >
+      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+      <div className="fixed inset-0 flex flex-col items-center justify-center p-4">
+        <Dialog.Panel className="w-full h-80 max-w-sm rounded-lg bg-white flex flex-col justify-start py-3 items-center">
+          <Dialog.Title className="text-xl">Add a suggestion</Dialog.Title>
+          <Combobox value={selectedSong} onChange={setSelectedSong} nullable>
+            <Combobox.Input
+              onChange={(event) => setQuery(event.target.value)}
+              className="shadow-md border-2 rounded-md pl-2 border-emerald-400 focus:ring-2 focus:border-none active:ring-0"
+              placeholder="Enter a song name"
+            />
+            <Combobox.Options className="px-4 w-full">
+              {filteredResults.map(({ name, id, artists }) => (
+                <Combobox.Option
+                  key={id}
+                  value={id}
+                  className="border-b flex flex-row border-b-neutral-300 w-full overflow-x-hidden"
+                >
+                  {name} - {artists}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
+          </Combobox>
+        </Dialog.Panel>
+      </div>
+    </Dialog>
+  );
+}
+
+/**
+ * deeply obliged, Marcelo
+ * https://gist.github.com/marcelo-ribeiro/abd651b889e4a20e0bab558a05d38d77
+ * thank you for allowing me to find Björk.
+ */
+const removeAccents = (text: string) =>
+  text.normalize("NFD").replace(/\p{Diacritic}/gu, "");
+
+const testQuery = (query: string, test_string: string) => {
+  const q = new RegExp(query.toLowerCase().replaceAll(" ", ".*") + ".*");
+  const test_against = removeAccents(test_string).toLowerCase();
+  return q.test(test_against);
+};
--- pages/jams.tsx
+++ pages/jams.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import ListeningTo from "../components/listeningTo";
+import Queue from "../components/queue";
 import TopTracks from "../components/topTracks";
 import { Song } from "./api/lib/spotify.d";
+import { fetchCurrentlyPlaying, fetchQueue, fetchTopTracks } from "./services";
 
 export type ITopTracks = Pick<Song, "name" | "href"> & { artists: string[] };
 export type ICurrentlyPlaying = ITopTracks & { is_playing: boolean };
@@ -9,14 +11,15 @@ export type ICurrentlyPlaying = ITopTracks & { is_playing: boolean };
 interface JamProps {
   tracks: ITopTracks[];
   listening_to: ICurrentlyPlaying;
+  queue: Song[];
 }
 
-export default function Jams({ tracks, listening_to }: JamProps) {
+export default function Jams({ tracks, listening_to, queue }: JamProps) {
   const [loading, setLoading] = React.useState(true);
 
   useEffect(() => {
-    if (tracks && listening_to) setLoading(false);
-  }, [tracks, listening_to]);
+    if (tracks && listening_to && queue) setLoading(false);
+  }, [tracks, listening_to, queue]);
 
   if (loading) {
     return (
@@ -31,23 +34,33 @@ export default function Jams({ tracks, listening_to }: JamProps) {
       <h1 className="text-3xl">Jared&apos;s Jams</h1>
       <ListeningTo currently_playing={listening_to} />
       <TopTracks tracks={tracks} />
+      <Queue queue={queue}/>
     </div>
   );
 }
 
 export async function getServerSideProps() {
-  const BASE_API_URL =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : "https://jaredjewell.dev";
+  // const BASE_API_URL =
+  //   process.env.NODE_ENV === "development"
+  //     ? "http://localhost:3000"
+  //     : "https://jaredjewell.dev";
+  //
+  // const { tracks } = await fetch(`${BASE_API_URL}/api/top-tracks`).then((d) =>
+  //   d.json()
+  // );
+  //
+  // const currently_playing = (await fetch(
+  //   `${BASE_API_URL}/api/currently-playing`
+  // ).then((d) => d.json())) as ICurrentlyPlaying;
+  //
+  // const queue = (await fetch(`${BASE_API_URL}/api/get-queue`).then((d) =>
+  //   d.json()
+  // )) as Song[];
+  //
+  // console.log(queue)
+  const tracks = await fetchTopTracks()
+  const currently_playing = await fetchCurrentlyPlaying()
+  const queue = await fetchQueue()
 
-  const { tracks } = await fetch(`${BASE_API_URL}/api/top-tracks`).then((d) =>
-    d.json()
-  );
-
-  const currently_playing = (await fetch(
-    `${BASE_API_URL}/api/currently-playing`
-  ).then((d) => d.json())) as ICurrentlyPlaying;
-
-  return { props: { tracks, listening_to: currently_playing } };
+  return { props: { tracks, listening_to: currently_playing, queue } };
 }
