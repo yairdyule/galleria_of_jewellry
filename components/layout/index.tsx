9a84a09b6608745cd9edd928a607588a5d9394d8
Jared Jewell <jaje9434@colorado.edu>
2022-05-28 23:40:12 -0600

    make width consistent on >md

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -22,7 +22,7 @@ export default function Layout({ children }: LayoutProps) {
         <meta name="description" content="A web-dev extraordinaire" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="min-h-screen max-w-2xl px-8 py-16 flex flex-1 flex-col justify-start items-center">
+      <main className="min-h-screen md:w-[570px] px-8 py-16 flex  flex-col justify-start items-center">
         <BreadCrumbs path={path} />
         {children}
       </main>
